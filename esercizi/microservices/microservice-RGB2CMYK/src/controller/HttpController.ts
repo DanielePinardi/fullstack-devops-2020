82015c600af520e944874cdc99fea21a3664c6e1
Daniele <pinardi.daniele01@gmail.com>
2022-04-22 16:52:26 +0200

    fix RGB2CMYK

--- esercizi/microservices/microservice-RGB2CMYK/src/controller/HttpController.ts
+++ esercizi/microservices/microservice-RGB2CMYK/src/controller/HttpController.ts
@@ -5,7 +5,7 @@ import { TtfRgb, TtfCmyk } from "../../../commons/src/model/Color"
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color = JSON.parse(req.query.color) as TtfRgb;
+            const color = JSON.parse(req.query.color as string) as TtfRgb;
             const convertedColor: TtfCmyk = convert(color);
 
             res.send(convertedColor);
